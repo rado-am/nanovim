b2e068ec56c192b0b1c74decc0d937fff50e7944
Radosław Łuczak <radoam@192.168.1.4>
2024-02-25 09:02:54 +0100

    Set base colors

---
+++ colors/nano.vim
@@ -0,0 +1,6 @@
+hi Normal guibg=#292c3e guifg=#ffffff80
+hi Visual guibg=#54566f
+hi Comment guifg=#6b6c6f gui=italic
+hi LineNr guifg=#858585
+hi CursorLineNr guifg=#b0b1ff
+hi CursorLine guibg=#232534
---
+++ init.lua
@@ -0,0 +1,5 @@
+vim.cmd.colorscheme("nano") -- Set nano color scheme
+vim.opt.termguicolors = true -- Set real colors
+vim.opt.nu = true -- Set line numbers
+vim.opt.relativenumber = true -- Set relative number
+vim.opt.cursorline = true -- Set cursor line to number (dont highlight all line)
