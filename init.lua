1656118095fc3843f59bc799a56a70d702c7eb4b
Radosław Łuczak <radoam@192.168.1.4>
2024-02-26 19:29:20 +0100

    Change StatusLine color

--- init.lua
+++ init.lua
@@ -4,4 +4,4 @@ vim.opt.nu = true -- Set line numbers
 vim.opt.relativenumber = true -- Set relative number
 vim.opt.cursorline = true -- Set cursor line to number (dont highlight all line)
 vim.opt.hlsearch = true -- Set highlight search
-vim.opt.incsearch = true 
+vim.opt.incsearch = true
