d874242402a015bbdb23a5f135c8ce35ea1372dd
Radosław Łuczak <radoam@192.168.1.4>
2024-02-26 19:35:04 +0100

    Delete ~ symbol for empty lines

--- init.lua
+++ init.lua
@@ -4,4 +4,5 @@ vim.opt.nu = true -- Set line numbers
 vim.opt.relativenumber = true -- Set relative number
 vim.opt.cursorline = true -- Set cursor line to number (dont highlight all line)
 vim.opt.hlsearch = true -- Set highlight search
-vim.opt.incsearch = true
+vim.opt.incsearch = true -- Set incremental search
+vim.opt.fillchars = { eob = " " } -- Delete "~" symbol 
