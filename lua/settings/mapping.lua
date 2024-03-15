1b6870d040d1288449928b4341a362939105cc46
Radosław Łuczak <radoam@192.168.1.4>
2024-03-15 11:29:06 +0100

    Add some mappings

--- lua/settings/mapping.lua
+++ lua/settings/mapping.lua
@@ -1,6 +1,13 @@
 local map = require("nanovim.core.utils").map
 
+-- General
 map("n", "<C-s>", ":w<cr>", { desc = "Save file" })
+map("n", "<C-a>", "ggVG", { desc = "Select all" })
+map("n", "<C-d>", "<cmd>t.<cr>", { desc = "Duplicate line" })
+map("n", "<esc>", "<cmd>noh<cr>", { desc = "Clear highlights" })
+map("x", "<C-c>", '"+y', { desc = "Copy selected text to system clipboard" })
 
+-- Telescope
 map("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Search everywhere" })
 map("n", "<cs-f>", "<cmd>Telescope live_grep<cr>", { desc = "Find in files" })
+map("n", "<C-f>", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Find in current file" })
