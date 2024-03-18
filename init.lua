56d225deab4f5954c8e650bc5b8ec62bc93900db
Radosław Łuczak <radoam@Radosaws-MacBook-Pro.local>
2024-03-18 22:41:59 +0100

    Move neovim opts to options file

--- init.lua
+++ init.lua
@@ -1,16 +1,4 @@
-vim.opt.termguicolors = true -- Set real colors
-vim.opt.nu = true -- Set line numbers
-vim.opt.relativenumber = true -- Set relative number
-vim.opt.cursorline = true -- Set cursor line to number (dont highlight all line)
-vim.opt.hlsearch = true -- Set highlight search
-vim.opt.incsearch = true -- Set incremental search
-vim.opt.fillchars = { eob = " " } -- Delete "~" symbol
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
 require("nanovim.core").lazy_setup()
 require("nanovim.core.kbinder").setup()
 require("settings.mapping")
+require("settings.options")
---
+++ lua/settings/options.lua
@@ -0,0 +1,11 @@
+vim.opt.termguicolors = true -- Set real colors
+vim.opt.nu = true -- Set line numbers
+vim.opt.relativenumber = true -- Set relative number
+vim.opt.hlsearch = true -- Set highlight search
+vim.opt.incsearch = true -- Set incremental search
+vim.opt.fillchars = { eob = " " } -- Delete "~" symbol
+vim.opt.tabstop = 2 -- Number of spaces that <Tab> counts for
+vim.opt.shiftwidth = 2 -- Automaticaly indent after new line (number of spaces)
+vim.opt.softtabstop = 2 -- Number of spaces cursor move right
+vim.opt.expandtab = true -- Use the appropriate number of spaces in insert mode
+vim.opt.smartindent = true -- Smart autoindenting when starting new line
